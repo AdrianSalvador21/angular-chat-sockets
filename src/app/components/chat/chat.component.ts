8dd3db4bee1a4dba8a3f8c6f7b04196e81c1dcc6
Adrian Salvador <adrian@smartlending.mx>
2019-12-27 16:42:11 -0600

    basic-sockets-implementation

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ChatService} from '../../services/chat.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit, OnDestroy {
+  text = '';
+  mensajes: any[];
+  elemento: HTMLElement;
+  mensajesSubscription: Subscription;
+
+  constructor(public chatService: ChatService) { }
+
+  ngOnInit() {
+    this.elemento = document.getElementById('chat-mensajes');
+    this.mensajesSubscription = this.chatService.getMessages().subscribe(msg => {
+      console.log(msg);
+      this.mensajes.push(msg);
+      setTimeout(() => {
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.mensajesSubscription.unsubscribe();
+  }
+
+  sendMessage() {
+    console.log(this.text);
+    if (this.text.trim().length === 0) {
+      return;
+    }
+    this.chatService.sendMessage(this.text);
+    this.text = '';
+  }
+
+
+
+}
